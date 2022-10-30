2eb0a7faceb4ba5df392bb0f64e9a0438d00ca09
anamoraisdev <anamorais.dev@gmail.com>
2022-10-30 13:47:42 -0300

    Criação da Newsletter

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,21 +10,22 @@ const Navbar = () => {
     }
   }
   return (
-    <div className='bg-[#226F9E] h-[100px] flex justify-between px-4 items-center'>
+    <div className='bg-secundaria h-[100px] flex justify-between px-4 items-center'>
         <img src="./logo.png" alt="logo" className='h-[60%]'/>
         <ul className='hidden md:flex text-white gap-8 items-center h-full'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Descrição</a></li>
-            <li><a href='#'>Produto</a></li>
+            <li className='hover:scale-110'><a href='#'>Home</a></li>
+            <li className='hover:scale-110'><a href='#'>Descrição</a></li>
+            <li className='hover:scale-110'><a href='#'>Produto</a></li>
         </ul>
+        {/* Inicio do menu mobile */}
         <div className='flex md:hidden text-white'>
           <button onClick={() => abrirMenu()}><img src="./menu.png" alt="menu" className='h-[50px]'/></button>
         </div>
         <div className={`${menuAberto ? 'flex md:hidden' : 'hidden'} right-0 absolute top-[100px] bg-[#226F9E]`}>
          <ul className='flex flex-col text-white gap-3 items-center h-full w-[200px]'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Descrição</a></li>
-            <li><a href='#'>Produto</a></li>
+            <li className='hover:scale-110'><a href='#'>Home</a></li>
+            <li className='hover:scale-110'><a href='#'>Descrição</a></li>
+            <li className='hover:scale-110'><a href='#'>Produto</a></li>
          </ul>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secundaria: "#486488",
+        terceira: "#06215A"
+      }
+    },
   },
   plugins: [],
 };
